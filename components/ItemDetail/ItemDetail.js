03f9b8edd8822942f7d6cb268a5d23a66aac68d9
Jesus Angel <jesus.feliciano@coppel.com>
2021-09-02 01:00:47 -0600

    added PropTypes to ItemDetail component

--- components/ItemDetail/ItemDetail.js
+++ components/ItemDetail/ItemDetail.js
@@ -1,14 +1,16 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable react/require-default-props */
 import Image from 'next/image';
 import CurrencyFormat from 'react-currency-format';
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './ItemDetail.module.scss';
+
 /**
  * Component to show the detailed view of the item
  * @param props - props object of react component
  * @param props.item - item object with description to show for detail
  */
-export default function ItemDetail({ item }) {
+function ItemDetail({ item }) {
   return (
     <div className={`grid-container-10-cols ${styles.itemDetailMainContainer}`}>
       <div className="col-7">
@@ -39,3 +41,21 @@ export default function ItemDetail({ item }) {
     </div>
   );
 }
+
+ItemDetail.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.shape({
+      currency: PropTypes.string.isRequired,
+      amount: PropTypes.number.isRequired,
+      decimals: PropTypes.number.isRequired,
+    }),
+    picture: PropTypes.string.isRequired,
+    condition: PropTypes.string.isRequired,
+    free_shipping: PropTypes.bool.isRequired,
+    sold_quantity: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+  }),
+};
+export default ItemDetail;
